0e235e3d10a90c68c7edd7297850ad31ab81b45c
TTolga9 <47156577+TTolga9@users.noreply.github.com>
2019-02-20 21:59:37 +0300

    Add files via upload

--- my_task/index.php
+++ my_task/index.php
@@ -1,35 +1,18 @@
 <?php
-require_once('controller.php');
-require_once('model.php');
 
-@$ent=$_REQUEST['ent'];
-
-$user_controller=new UserController();
-
-switch ($ent) {
-	case 'Login':
-		$username = $_POST['user_name'];
-		$password = $_POST['user_pass'];
-
-		if($user_controller->login($username, $password)){
-			header("Location:main.php");
-		} else {
-		 echo '<script type="text/javascript">alert("Wrong username or password!");</script> <meta http-equiv="refresh" content="0;URL=login.php" />';
-			//header("Location:login.php");
-		}
-
-		break;
-		
-	case 'logout':
-		$user_controller->logout();
-		header("Location:login.php");
-		break;
-
-		
-	
-	default:
-		header("Location:login.php");
-		break;
-}
+require __DIR__ . '/control.php';
+require __DIR__ . '/model.php';
+require __DIR__ . '/controller.php';
+require __DIR__ . '/route.php';
+require __DIR__ . '/main.php';
+require __DIR__ . '/login.php';
+Route::run('/', function () {
+    echo 'merhaba!';
+});
+Route::run('/controller', 'controller@create');
+Route::run('/controller', 'controller@login');
+Route::run('/uyeler', 'uyeler@post', 'post');
+Route::run('/uye/{url}', 'uye@index');
+Route::run('/profil/sifre-degistir', 'profile/changepassword@index');
 
 ?>
